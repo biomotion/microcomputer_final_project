1bf3b49028f2a533d9726bcd1f74cf8c4e7d1eb9
biomotion <bensonpuppy1997@gmail.com>
2020-06-30 01:28:15 +0800

    import GLCD library

--- Source/main.c
+++ Source/main.c
@@ -1,5 +1,20 @@
 #include "stm32f4xx.h"
+#include "stm324xg_lcd_sklin.h"
+#include "screen_control.h"
+#include <stdio.h>
+
+void stm32f4_Hardware_Init(void);
 
 int main(void){
-	return 0;
+	stm32f4_Hardware_Init();
+	// Initilize GPIO
+	
+	// Show Welcome Message
+	LCD_Init();
+	LCD_DisplayOn();
+	
+  LCD_Clear(RED);
+	
+	// Loop for Playing
+	
 }
---
+++ Source/screen_control.h
@@ -0,0 +1,2 @@
+#include "stm32f4xx.h"
+#include "stm324xg_lcd_sklin.h"
